d7dda9277d8e6753d5a07954d3d5898e67c42999
Mike Saint-Antoine <mikesaint-antoine@wifi-roaming-128-4-126-58.wifi.udel.edu>
2021-08-27 15:24:33 -0400

    add python basics

---
+++ python_basics/learning_python.py
@@ -0,0 +1,43 @@
+
+
+### defining a list
+##my_list = [3,2,7.0,"mike"]
+##
+##print(my_list)
+##
+##print(my_list[2])
+##
+##
+##for i in range(0,5):
+##
+##    for j in range(0,5):
+##
+##        print(i*j)
+##
+
+
+
+##test = 2 < 1
+##
+##print(test)
+
+
+
+##if 2 < 1 :
+##    print("this is true")
+##
+##elif 2 > 1:
+##    print("test")
+##    
+##else:
+##    print("else test")
+
+
+x = 0
+
+while x < 10:
+    x = x + 1
+    print(x)
+
+print("finished")
+
